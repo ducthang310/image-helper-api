700498768600372145200d61bffdbb9e096c8870
Thang <ducthang310@gmail.com>
2020-07-25 22:56:01 +0700

    Lib: Multer - for handling file upload

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const downloader = require('../app/services/file.service');
 const rimraf = require('rimraf');
 const archiver = require('archiver');
+const multer = require('multer');
+const upload = multer({ dest: 'storage/multer' });
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/download', async (req, res, next) => {
   const {url, number} = req.body;
   try {
     const folderName = await downloader.download(url, number);
@@ -27,6 +29,35 @@ router.post('/', async (req, res, next) => {
     archive.finalize();
   } catch (e) {
     console.log(e.message);
+    res.sendStatus(500) && next(e);
+  }
+});
+
+router.post('/compress', upload.single('images'), async (req, res, next) => {
+  try {
+    const name = uuid();
+    // Store zip file into `temp` folder:
+    const file = req.file;
+    const tempFolderPath = __basedir + '/storage/temp/';
+    const imagesPath =  tempFolderPath + name;
+
+    // Unzip to imagesPath
+
+
+    const archive = archiver('zip', { zlib: { level: 9 }});
+    archive.directory(imagesPath, false);
+    res.set({
+      'Content-Type': 'application/zip',
+      'Content-Disposition': 'attachment; filename="' + folderName + '.zip' + '"'
+    });
+    res.on('finish', function () {
+      console.log('Finished sending coverage data.');
+      rimraf(folderPath, () => {});
+    });
+    archive.pipe(res);
+    archive.finalize();
+  } catch (e) {
+    console.error(e.message);
     res.sendStatus(500) && next(e);
   }
 });
